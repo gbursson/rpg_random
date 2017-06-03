6303200edb3f33aed9e768e5696abaef97968c15
Gbursson <gbursson@gmail.com>
2017-06-03 15:52:28 +0100

    added sqlite db reading random item from db removed unneeded files

---
+++ adventure.py
@@ -0,0 +1,8 @@
+import random
+import sqlite3 as db
+
+conn = db.connect('adventures.sqlite')
+c = conn.cursor()
+result = c.execute("SELECT * from THEMES")
+theme = random.choice(result.fetchall())
+print(theme[1], '\n', theme[2])
