b6876dc7e2604055b543c46daec61d1c87933d57
Gbursson <gbursson@gmail.com>
2017-06-05 01:40:05 +0100

    working prototype for randomly selecting an entry and writing everything into HTML file.

--- adventure.py
+++ adventure.py
@@ -1,13 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import random
 import sqlite3 as db
 
 conn = db.connect('adventures.sqlite')
 c = conn.cursor()
 
-source = {"themes": "Głowny temat", "villains": "Przeciwnik", "minor_villains": "Pomocnik")}
+source = {"themes": "Glowny temat", "villains": "Przeciwnik", "minor_villains": "Pomocnik"}
 
-for tableName[0] in source:
-    query = "SELECT * FROM {0}".format(tableName[0])
-# debug
-queryResult = c.execute(query)
-tableName = random.choice(queryResult.fetchall())
-print(tableName[0])
+for dictIndex in source:
+    query = "SELECT * FROM {0}".format(dictIndex)
+    queryResult = c.execute(query)
+    item = random.choice(queryResult.fetchall())
+    name = item[0]
+    desc = item[1]
+    htmlOutput = "<b>{0}</b>: ".format(source[dictIndex]) + name
+    htmlOutput = htmlOutput + "<br><b>Opis</b>: " + "{0}".format(desc) + ""
+    htmlOutput = str(htmlOutput)
+
+    print(htmlOutput)
+
+    # htmlFile = open("adv.html", encoding="utf-8", mode="r+")
+    # htmlFile.write(htmlOutput)
